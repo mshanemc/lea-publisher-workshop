984f4e1b7f4e1f8fe8bc637cd6a114abf9d02f6c
mshanemc <shane.mclaughlin@salesforce.com>
2020-08-05 14:56:10 -0500

    setup for community creation

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,9 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
-sfdx force:source:push
 # install mobile publisher managed packages https://help.salesforce.com/articleView?id=branded_apps_playground_getstarted.htm&type=5
 sfdx force:package:install --package 04t1I000003RkFQQA0 --publishwait 60 --wait 60
 sfdx force:package:install --package 04t1I000003RkFaQAK --publishwait 60 --wait 60
+sfdx shane:github:package:install -g mshanemc -r community-boilerplate
+
+sfdx force:source:push
 
 sfdx force:org:open
